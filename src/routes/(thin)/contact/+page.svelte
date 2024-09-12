6fd9080b29e5b2e3b5d00e5c12471dbafe2ae39f
Lewin Kelly <heli@odex.cf>
2024-09-12 09:49:46 +0100

    Improve contact links and add unused profiles

--- src/routes/(thin)/contact/+page.svelte
+++ src/routes/(thin)/contact/+page.svelte
@@ -59,6 +59,26 @@
 		],
 		["Email", "Heli@odex.cf", "mailto:heli@odex.cf", "heli@odex.cf"],
 	]
+	const deadlinks = [
+		[
+			"X formerly Twitter",
+			"@lwinklly",
+			"https://twitter.com/lwinklly/",
+			"twitter.com/lwinklly",
+		],
+		[
+			"Bluesky",
+			"npub1lllllll9xdgqvp5l3drml7kwrre9u5ma6je7ey8r0578dfqq8l8qrjujua.nostr.heliodex.cf",
+			"https://bsky.app/profile/npub1lllllll9xdgqvp5l3drml7kwrre9u5ma6je7ey8r0578dfqq8l8qrjujua.nostr.heliodex.cf",
+			"bsky.app/profile/npub1lllllll9xdgqvp5l3drml7kwrre9u5ma6je7ey8r0578dfqq8l8qrjujua.nostr.heliodex.cf",
+		],
+		[
+			"Instagram",
+			"@heliodex",
+			"https://www.instagram.com/heliodex/",
+			"instagram.com/heliodex",
+		],
+	]
 </script>
 
 <Head title="Contact" />
@@ -70,12 +90,32 @@
 </p>
 <table
 	in:fly={{ y: 40 }}
-	class="bg-#0008 @light:bg-#fffb backdrop-blur-4px rounded-4 border-#fff1 @light:border-#0003 mt-6 w-full border border-solid p-4">
+	class="bg-#0008 @light:bg-#fffb backdrop-blur-4px rounded-4 border-#fff1 @light:border-#0003 w-full border border-solid p-4">
 	{#each links as [name, user, href, link]}
 		<tr>
-			<td>{name}</td>
-			<td>{user}</td>
-			<td>
+			<td class="min-w-10rem max-w-10rem overflow-hidden">{name}</td>
+			<td class="min-w-8rem max-w-8rem overflow-hidden">{user}</td>
+			<td class="min-w-44rem max-w-44rem overflow-hidden">
+				<a {href} target="_blank" rel="noopener noreferrer">
+					{link}
+				</a>
+			</td>
+		</tr>
+	{/each}
+</table>
+<p>
+	For reference, here are some media that I have abandoned or no longer use.
+	<br />
+	I'm very unlikely to respond to queries through these contacts.
+</p>
+<table
+	in:fly={{ y: 40 }}
+	class="bg-#0008 @light:bg-#fffb backdrop-blur-4px rounded-4 border-#fff1 @light:border-#0003 w-full border border-solid p-4">
+	{#each deadlinks as [name, user, href, link]}
+		<tr>
+			<td class="min-w-10rem max-w-10rem overflow-hidden">{name}</td>
+			<td class="min-w-8rem max-w-8rem overflow-hidden">{user}</td>
+			<td class="min-w-44rem max-w-44rem overflow-hidden">
 				<a {href} target="_blank" rel="noopener noreferrer">
 					{link}
 				</a>
